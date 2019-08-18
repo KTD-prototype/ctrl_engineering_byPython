9847fac28adef1b6b397321ed1f1748f967cee90
k24koba_laptop <hozu.kazu.hozu@gmail.com>
2019-08-18 09:50:05 +0900

    initial commit

---
+++ 2.3.4_generator.py
@@ -0,0 +1,10 @@
+def linestyle_generator():
+    linestyle=['-','--','-.',':']
+    lineID=0
+    while True:
+        yield linestyle[lineID]
+        lineID=(lineID+1)%len(linestyle)
+
+LS=linestyle_generator() #make a generator : "LS"
+for i in range(5):
+    print(next(LS))
